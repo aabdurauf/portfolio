e94437336e84e2e006f9ac1c324fdc500f0497a1
aabdurauf <aabdurauf004@gmail.com>
2026-01-08 18:12:04 +0500

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
         <a href="https://www.instagram.com/abdurauf_a_/" className="w-10">
           <img src={instagram} alt="instagram_icon" />
         </a>
-        <a href="https://t.me/abduraufss" className="w-10">
+        <a href="https://t.me/just_abdu" className="w-10">
           <img src={telegram} alt="telegram_icon" />
         </a>
         <a href="https://www.linkedin.com/in/abdurauf-ashurov-656415213/" className="w-10">
